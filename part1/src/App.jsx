b1ac2a8a4a0237f70658da8d05c6c026555fc84a
Nicolas Basso <bassonicolasnjbl@gmail.com>
2023-12-30 01:59:13 -0300

    part 1 completed

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,5 +1,6 @@
+import { useState } from "react";
+
 const Header = (props) => {
-  console.log(props);
   return (
     <>
       <h1>{props.course}</h1>
@@ -8,40 +9,85 @@ const Header = (props) => {
 };
 
 const Content = (props) => {
-  console.log(props);
   return (
     <>
       <p>
-        part1: {props.part1}
-        <br></br> 
-        exercises: {props.exercises1}
-      </p>
-      <p>
-        part2: {props.part2} 
-        <br></br>
-        exercises: {props.exercises2}
-      </p>
-      <p>
-        part3: {props.part3}
-        <br></br>
-        exercises: {props.exercises3}
+        {props.text} {props.value}
       </p>
     </>
   );
 };
 
 const Total = (props) => {
-  console.log(props);
-  const totalExercises = props.exercises1 + props.exercises2 + props.exercises3;
-
   return (
     <>
-      <p>Total exercises: {totalExercises}</p>
+      <p>
+        {props.text} {props.value}
+      </p>
     </>
   );
 };
 
+const StatisticsLine = (props) => {
+  return (
+    <>
+      <tr>
+        <td>{props.text}</td>
+        <td>{props.value}</td>
+      </tr>
+    </>
+  );
+};
+
+const Statistics = (props) => {
+  if (props.all) {
+    return (
+      <>
+        <h2>statistics</h2>
+        <table>
+          <tbody>
+            <StatisticsLine text="good" value={props.good} />
+            <StatisticsLine text="Neutral" value={props.neutral} />
+            <StatisticsLine text="Bad" value={props.bad} />
+            <StatisticsLine text="all" value={props.all} />
+            <StatisticsLine text="avergae" value={props.average} />
+            <StatisticsLine text="positive" value={`${props.positive}%`} />
+          </tbody>
+        </table>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <p>No feedback given</p>
+      </>
+    );
+  }
+};
+
+const Button = (props) => {
+  return (
+    <>
+      <button onClick={props.value}>{props.text}</button>
+    </>
+  );
+};
+
+const MostVotedAnecdote = (props) => {
+  return (
+    <>
+      <p>{props.value}</p>
+    </>
+  )
+}
+
 const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  const [all, setAll] = useState(0);
+  const [selected, setSelected] = useState(0);
+
   const course = {
     name: "Half Stack application development",
     parts: [
@@ -60,23 +106,104 @@ const App = () => {
     ],
   };
 
+  const totalExercises =
+    course.parts[0].exercises +
+    course.parts[1].exercises +
+    course.parts[2].exercises;
+
+  const handleGoodClick = () => {
+    setGood(good + 1);
+    const updateGood = good + 1;
+    setGood(updateGood);
+    setAll(updateGood + neutral + bad);
+  };
+  const handleNeutralClick = () => {
+    setNeutral(neutral + 1);
+    const updateNeutral = neutral + 1;
+    setNeutral(updateNeutral);
+    setAll(good + updateNeutral + bad);
+  };
+  const handleBadClick = () => {
+    setBad(bad + 1);
+    const updateBad = bad + 1;
+    setBad(updateBad);
+    setAll(good + neutral + updateBad);
+  };
+
+  const average = all / 3;
+  const positive = (good / all) * 100;
+
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+
+  const [votes, setVotes] = useState(anecdotes.map(() => 0));
+
+  const randomAnecdote = () => {
+    const randomIndex = Math.floor(Math.random() * anecdotes.length);
+    setSelected(randomIndex);
+  };
+
+  const handleVoteClick = () => {
+    const newVotes = [...votes];
+    newVotes[selected] += 1;
+    setVotes(newVotes);
+  };
+
+  const mostVotedAnecdote = () => {
+    const mostVotedIndex = votes.indexOf(Math.max(...votes));
+    return mostVotedIndex
+  };
+
   return (
-    <div>
-      <Header course={course.name} />
-      <Content
-        part1={course.parts[0].name}
-        exercises1={course.parts[0].exercises}
-        part2={course.parts[1].name}
-        exercises2={course.parts[1].exercises}
-        part3={course.parts[2].name}
-        exercises3={course.parts[2].exercises}
-      />
-      <Total
-        exercises1={course.parts[0].exercises}
-        exercises2={course.parts[1].exercises}
-        exercises3={course.parts[2].exercises}
-      />
-    </div>
+    <>
+      <div>
+        <Header course={course.name} />
+        <Content
+          text={course.parts[0].name}
+          value={course.parts[0].exercises}
+        />
+        <Content
+          text={course.parts[1].name}
+          value={course.parts[1].exercises}
+        />
+        <Content
+          text={course.parts[2].name}
+          value={course.parts[2].exercises}
+        />
+        <Total text="Total exercises:" value={totalExercises} />
+      </div>
+      <div>
+        <h1>Give feedback</h1>
+        <Button text="Good" value={handleGoodClick} />
+        <Button text="Neutral" value={handleNeutralClick} />
+        <Button text="Bad" value={handleBadClick} />
+        <Statistics
+          good={good}
+          neutral={neutral}
+          bad={bad}
+          all={all}
+          average={average}
+          positive={positive}
+        />
+      </div>
+      <div>
+        <h1>acnedote</h1>
+        <p>{anecdotes[selected]}</p>
+        <button onClick={handleVoteClick}>Vote</button>
+        <p>votes: {votes[selected]}</p>
+        <button onClick={randomAnecdote}>Next acnedote</button>
+        <h1>Anecdote with most votes</h1>
+        <MostVotedAnecdote value={anecdotes[mostVotedAnecdote()]}/>
+      </div>
+    </>
   );
 };
 
